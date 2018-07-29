da1dde02e4a65d05418f124983190eaaa251d957
Amanda <aleisero@ucsc.edu>
2018-07-29 15:34:10 -0700

    complete but w/out fixing overlap, etc

--- ManyRooms.js
+++ ManyRooms.js
@@ -1,6 +1,5 @@
 //ManyRooms interactive 2D space generation project by Amanda Leiserowitz
 
-
 //declare global variables
 
 var wallArray;
@@ -9,7 +8,8 @@ var floorArray;
 var door1, door2, door3;
 var plant1, plant2, plant3;
 var paint1, paint2, paint3;
-var mat, spider;
+var mat, spider, shoe;
+var puddle1, puddle2, puddle3;
 
 function setup() {
 	createCanvas(640, 640);
@@ -25,9 +25,15 @@ function setup() {
 	paint3 = loadImage("assets/paint3.jpg");
 	mat = loadImage("assets/mat.png");
 	spider = loadImage("assets/spider.png");
+	shoe = loadImage("assets/shoe.png");
+	puddle1 = loadImage("assets/puddle1.png");
+	puddle2= loadImage("assets/puddle2.png");
+	puddle3 = loadImage("assets/puddle3.png");
+	rug1 = loadImage("assets/rug1.png");
+	rug2 = loadImage("assets/rug2.png");
 	
 	//put all assets in appropriate array
-	floorArray = [mat, spider];
+	floorArray = [mat, spider, shoe, puddle1, puddle2, puddle3, rug1, rug2];
 	wallArray = [door1, door2, door3, plant1, plant2, plant3, paint1, paint2, paint3];
 	
 	//draw BG
@@ -64,24 +70,27 @@ function resetSketch() {
 }
 
 function wallDraw(){
-	//choose a random wall asset and draw it at mouseX, mouseY or mouseX, 70 depending on the type of object
+	//choose a random wall asset
 	var temp = random(wallArray);
 	
 	//check if it is a door
+	//if so, place it touching floor
 	if (temp == door1 || temp == door2 || temp == door3) {
 		image(temp, mouseX, 70);
 	}
 	//check if it's a potted plant
+	//if so, place it touching floor
 	else if (temp == plant1 || temp == plant2 || temp == plant3) {
 		image(temp, mouseX, 245);
 	}
+	//if it's a painting place it at mouseX, mouseY
 	else {
 		image(temp, mouseX, mouseY);
 	}
 }
 
 function floorDraw() {
-	//choose a random floor asset and draw it at mouseX,
+	//choose a random floor asset and draw it at mouseX, mouseY
 	var temp = random(floorArray);
 	image(random(floorArray), mouseX, mouseY);
 }
